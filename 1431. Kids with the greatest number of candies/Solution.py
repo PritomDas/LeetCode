22f0145c898e081c42f91fadeef8b86b332a358d
PritomDas <das.pritom175@gmail.com>
2020-05-08 20:35:07 -0500

    Update Solution.py

--- 1431. Kids with the greatest number of candies/Solution.py
+++ 1431. Kids with the greatest number of candies/Solution.py
@@ -2,24 +2,20 @@
 
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        #calling the inbuilt max function to find the largest element from the list
+        MAX = max(candies)
         
         #making an empty list that will store the final answer for this problem
         output = []
         
-        #calling the inbuilt max function to find the largest element from the list
-        MAX = max(candies)
-        
         #comparing MAX with each element from the list candies
         #simultaneously appending either true or false based on the condition to output list
-        for elem in candies:
-            if (elem + extraCandies) >= MAX:
-                output.append("true")
+        for index in range(len(candies)):
+            if candies[index] + extraCandies >= MAX:
+                output.append("True")
             else:
-                output.append("false")
-        
-        #return the final list
-        return output
-    
+                output.append("False")
+        return output   
     
     #this code works
     class Solution:
