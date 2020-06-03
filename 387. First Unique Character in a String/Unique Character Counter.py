a62373c50529a5c4011b0f7737ba9df99218db29
PritomDas <das.pritom175@gmail.com>
2020-06-03 16:18:29 -0500

    Update Unique Character Counter.py

--- 387. First Unique Character in a String/Unique Character Counter.py
+++ 387. First Unique Character in a String/Unique Character Counter.py
@@ -13,3 +13,27 @@ class Solution:
             if count[ch] == 1:
                 return idx     
         return -1
+    
+    
+    #another solution
+    
+    from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        
+        #converting into lowercase
+        s = s.lower()
+        
+        #converting into a string type to list
+        s = list(s)
+        
+        #setting a counter map to count occurence of each character
+        counterMap = Counter(s)
+        
+        '''in the countermap: count 0 = no occurence of that alphabet
+                              count 1 = that character appeared only once
+                              count >1 = multpile occurence of that character'''
+        for idx,char in enumerate(s):
+            if counterMap[char] == 1:
+                return idx
+        return -1
